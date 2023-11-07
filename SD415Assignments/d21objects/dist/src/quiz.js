94fb73ed931aea15bcb7b5589a80580465316d24
abebewodaj <abebew2127@gmail.com>
2023-11-07 14:36:50 -0600

    objected ass commited

--- SD415Assignments/d21objects/dist/src/quiz.js
+++ SD415Assignments/d21objects/dist/src/quiz.js
@@ -1,18 +1,12 @@
-
 // export interface StudentQuiz {
 //     studentId: number,
 //     quizAnswers: number[]
 // }
-
+export {};
 // export function gradeStudent(studentAnswers: number[], correct: number[]): number {
 // //IMPLEMENT THIS
 // }
-
 // export function gradeQuiz(
 // //IMPLEMENT THIS
-
-
-
 // export function gradeQuizLabeled(
 // //IMPLEMENT THIS
-
